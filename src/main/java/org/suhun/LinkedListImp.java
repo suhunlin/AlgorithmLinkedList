50e8676d4bd3e2a746d28bfa2c083f135971ab7f
suhunlin <lin.suhun@msa.hinet.net>
2023-11-14 22:22:16 +0800

    Implement add function and search function

---
+++ src/main/java/org/suhun/LinkedListImp.java
@@ -0,0 +1,62 @@
+package org.suhun;
+
+public class LinkedListImp {
+    public class Node{
+        Integer val;
+        Node next;
+        public Node(int val){
+            this.val = val;
+        }
+    }
+    private Node start;
+    private Node end;
+
+    public void add(int val){
+        if(start == null){
+            start = new Node(val);
+            end = start;
+        }else{
+            end.next = new Node(val);
+            end = end.next;
+        }
+    }
+
+    public void printNode(){
+        if(start==null){
+            System.out.println("Node is empty!!!");
+            return;
+        }else{
+            System.out.println(start.val);
+            Node haveNext = start.next;
+            while(haveNext!=null){
+                System.out.println(haveNext.val);
+                haveNext = haveNext.next;
+            }
+        }
+    }
+
+    public Integer search(int val){
+        int count = 1;
+        if(start == null){
+            System.out.println("Node is empty!!!");
+            return null;
+        }
+        if(start.val == val){
+            System.out.println("Found " + start.val + ",total find "+count+" times!");
+            return start.val;
+        }else{
+            Node haveNext = start.next;
+            while(haveNext!=null){
+                count++;
+                if(haveNext.val == val){
+                    System.out.println("Found "+ haveNext.val+", total find "+ count + " times!!!");
+                    return haveNext.val;
+                }else{
+                    haveNext = haveNext.next;
+                }
+            }
+        }
+        System.out.println("Nothing Found !!!Total find "+ count + " times!!!");
+        return null;
+    }
+}
--- src/main/java/org/suhun/Main.java
+++ src/main/java/org/suhun/Main.java
@@ -2,6 +2,13 @@ package org.suhun;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
+        LinkedListImp linkedListImp = new LinkedListImp();
+        linkedListImp.add(10);
+        linkedListImp.add(20);
+        linkedListImp.add(30);
+        linkedListImp.add(40);
+        linkedListImp.printNode();
+        linkedListImp.search(40);
     }
 }
