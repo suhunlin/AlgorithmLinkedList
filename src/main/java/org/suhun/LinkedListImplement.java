41ee3411ac4d430fb8bd2ab4d3ed76b5ccb4b973
suhunlin <lin.suhun@msa.hinet.net>
2023-11-15 10:51:02 +0800

    Implement addNode and search method

---
+++ src/main/java/org/suhun/LinkedListImplement.java
@@ -0,0 +1,66 @@
+package org.suhun;
+
+public class LinkedListImplement {
+    class Node{
+        Integer val;
+        Node next;
+
+        public Node(int val){
+            this.val = val;
+        }
+    }
+    private Node start;
+    private Node end;
+
+    public void addNode(int value){
+        if (start == null) {
+            start = new Node(value);
+            end = start;
+        }else{
+            end.next = new Node(value);
+            end = end.next;
+        }
+    }
+
+    public void outputNode(){
+        Node node = start;
+        if(node!=null){
+            int count = 1;
+            while(true){
+                if(node==null) break;
+
+                System.out.println(count + ".Node value is "+ node.val);
+                node = node.next;
+                count++;
+            }
+
+        }else{
+            System.out.println("No Node to output!!!");
+        }
+    }
+
+    public Integer search(int value){
+        Node node = start;
+        if(start != null){
+            while(true){
+                if(node == null){
+                    System.out.println("已經搜尋到最後一個節點了，跳出迴圈!!!");
+                    break;
+                }
+
+                if(node.val == value){
+                    System.out.println("搜尋到"+node.val);
+                    return node.val;
+                }else{
+                    node = node.next;
+                }
+            }
+
+        }else{
+            System.out.println("一個節點都沒有，無法搜尋!!!");
+            return null;
+        }
+        System.out.println("沒有搜尋到任何節點有"+value+"的值!!!");
+        return null;
+    }
+}
--- src/main/java/org/suhun/Main.java
+++ src/main/java/org/suhun/Main.java
@@ -2,19 +2,14 @@ package org.suhun;
 
 public class Main {
     public static void main(String[] args) {
-//        System.out.println("Hello world!");
-        LinkedListImp linkedListImp = new LinkedListImp();
-        linkedListImp.add(10);
-        linkedListImp.add(20);
-        linkedListImp.add(30);
-        linkedListImp.add(40);
+        LinkedListImplement linkedListImplement = new LinkedListImplement();
+        linkedListImplement.addNode(10);
+        linkedListImplement.addNode(20);
+        linkedListImplement.addNode(30);
+        linkedListImplement.addNode(40);
+        linkedListImplement.addNode(50);
+        linkedListImplement.outputNode();
+        linkedListImplement.search(55);
 
-//        linkedListImp.search(20);
-//        linkedListImp.delete(30);
-        linkedListImp.add(50);
-        linkedListImp.add(60);
-        linkedListImp.delete(90);
-//        linkedListImp.delete(60);
-        linkedListImp.printNode();
     }
 }
